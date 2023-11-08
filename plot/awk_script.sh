4db41103d1c53c9154934bad6d241ce3e39a8915
caraortizmah <caraortizmah@gmail.com>
2023-11-08 16:51:30 +0100

    creating a generalized form, it can be absq or socabsq

--- plot/awk_script.sh
+++ plot/awk_script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # script to create .csv format to be used in python
+# pre can be "socabsq" or "absq"
+pre="$1"
 
-suf=".out.absq.dat"
-for vars in `ls *.out.absq.dat`
+suf=".out.${pre}.dat"
+for vars in `ls *.out.${pre}.dat`
 do
 	job=${vars/%$suf}
 	awk '{ printf "%s,%s,%s,%s,%s\n", $1, $2, $3, $4, $5  }' $vars > $job.csv
