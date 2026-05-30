220ef47a9702cf8c6b9477d86224d258ad24235d
caraortizmah <caraortizmah@gmail.com>
2026-05-30 13:10:02 +0200

    Add initialization and attributes to class Scheme

--- qm_etdac_calculator/etdac_calculator.py
+++ qm_etdac_calculator/etdac_calculator.py
@@ -1,74 +1,98 @@
-# %% [markdown]
-# #### Big data sampling of the standard post analysis
-# 
-# Data presented here, as new inputs for this jupyter-notebook, comes from the written-shell pipeline created to extract X-ray excited-state features from some specific pair of atoms group (e.g. pair amino acid). Outputs here are filtered features that account for specific transitions (coreMO -> virtualMO) for the specific pair of atoms group
+"""
+Scheme of running the electron transition decomposition atomic contribution
+(ETDAC) calculation.
 
-# %% [markdown]
-# ###### Check out the paper of this work <br>
-# ##### *X-ray absorption spectroscopy reveals charge transfer in π-stacked aromatic amino acids*:<br> https://doi.org/10.1039/D4CP04615C
+Data presented here comes from the xas-qmol-parser
+Read  https://doi.org/10.1039/D4CP04615C : 
+"X-ray absorption spectroscopy reveals charge transfer in π-stacked aromatic amino acids"
+for further information
+"""
 
-# %%
 import numpy as np
 import pandas as pd
 import csv as csv
-
-# %%
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# %%
 import h5py
 
+from .gate_controler import Loader
 
-# %% [markdown]
-# ______________________________________________
+class Scheme:
+    """
+    Take the accepted arguments from ConfigMan class and load the log files,
+    enables the ETDAC calculation and saves the results.
+    
+    This class acts as an overall scheme that calls other routines to perform
+    the calculation of the ETDAC analysis. 
+    
+    Mandatory parameters: log_files_name (3), paths (3)
+    Optional parameters: None
+    """
+    
+    def __init__(self, **kwargs: str):
+        """
+        After ConfigMan "approves" the existence of the provided arguments, the same 
+        received arguments pass to this class. Of course, if the files have an inner
+        problem that is responsibility of the user.
+        
+        kwargs:
+            
+            'corepop': corepop_log_file =  File of path lists having core MOs population files
+            'virtpop': virtualpop_log_file = File of path lists having virtual MOs population files
+            'etranscv': etrans_log_file = File of path lists having core-virtual e- transitions files
+            'logspath': logs_path = Path to the log files (execution path by defult)
+            'outputpath': output_path = Path to the H5 output file
+        """
+        self.corelog_file = kwargs['corepop']
+        self.virtualog_file = kwargs['virtpop']
+        self.etranslog_file = kwargs['etranscv']
+        self.logs_path = kwargs['logspath']
+        self.output_path = kwargs['outputpath']
+        self.errors: List[str] = []
+        self.warnings: List[str] = []
+        self.data: Dict[str, Dict] = {}
+        self.etdac: Dict = {}
 
-# %%
-#Data in .csv format
+        # Initialize classes
+        open_data = Loader()
 
-# %%
-# resA core MO - $$\boldsymbol{\text{N}}_{A,i} $$
-resa_mocore_raw = load_dict_data('resA_MOcore_list.log', 'num-1')
+    def get_info(self, log_file: str) -> {}:
+        if open_data.is_datawell(log_file):
+            return open_data.get_dict_data(log_file, 'num-1')
+        else:
+            self.errors.append(f"One or more problems with file {log_file} were detected. Do a diagnosis.\n")
+            return {}
 
-# %%
-# resB virt MO - $$\boldsymbol{\text{N}}_{A^{\prime},a} $$
-resb_movirt_raw = load_dict_data('resB_MOvirt_list.log', 'num-1')
+    def opendata(self) -> Bool:
+        self.data['corepop'] = self.get_info(self.corelog_file)
+        self.data['virtualpop'] = self.get_info(self.virtualog_file)
+        self.data['etrans'] =self.get_info(self.etranslog_file)
+        # if any of them is empty is because get_info found something wrong
+        if not (self.data['corepop'] or self.data['virtualpop'] or self.data['etrans']):
+            return False
+        return True
 
 
-# %%
-# Transition density state matrix (core/virt MO) 
-#  as function of the oscillator strength using electronic dipole moment as operator
-#  only presented by weighted (WSM): fosce
-#  $$ \gamma^{[l,m]}_{ia} $$
-corevirtMO_fosce_raw = load_dict_data('corevirt_fosce_AB_list.log', 'virt\core')
+#resa_mocore_raw = load_dict_data('resA_MOcore_list.log', 'num-1')
+#resb_movirt_raw = load_dict_data('resB_MOvirt_list.log', 'num-1')
+#corevirtMO_fosce_raw = load_dict_data('corevirt_fosce_AB_list.log', 'virt\core')
 
-# %%
 # Reducing Löwdin Population MO matrices by removing non-contributing MO
 
-# %%
 #  Case for core MO in the resA 
 resa_mocore = remove_noncontrb(resa_mocore_raw)
 
-# %%
 # Case for virtual MO in the resB
 resb_movirt = remove_noncontrb(resb_movirt_raw)
 
-# %% [markdown]
-# ___________________________
-
-# %% [markdown]
 # ### Results
 
-# %%
 """
 Final calculation: $$ \huge \tilde{\gamma}^{[l,m]}_{AA^{\prime}} $$
 """
 
-# %%
 # WSM case, it is the official representation by the domain-knowledge
 heatmap_raw = heatmap_ETDAC(resa_mocore, resb_movirt_raw, corevirtMO_fosce_raw)
 
-# %%
+
 for key in heatmap_raw.keys():
     heatmap_raw[key].index.names = ['core-atom']
     heatmap_raw[key].columns.names = ['virtual-atom']
